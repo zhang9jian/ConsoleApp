0e7793818c5adad804b47d3fb3bcc4aef6003239
jian zhang <18701689361@163.com>
2020-06-10 15:39:29 +0800

    rsa

--- Armour/Program.cs
+++ Armour/Program.cs
@@ -1,16 +1,63 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Armour
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            RSAUtils rsaUtils = new RSAUtils();
-            RSAUtils.RSASecretKey rsaKey = rsaUtils.GenerateRSASecretKey(1024);
-            string en = rsaUtils.RSAEncrypt(rsaKey.PublicKey, "我的咪咪");
-            Console.WriteLine(rsaUtils.RSADecrypt(rsaKey.PrivateKey, en));
+            rsaByGenKey(2048, "123");
+            rsaByRandomKey("中国人");
+        }
+
+        public static void rsaByGenKey(int size, string content)
+        {
+            RSASecretKey key = RSAUtils.GenerateRSASecretKey(size);
+            string en = RSAUtils.RSAEncrypt(key.PublicKey, content);
+            string de = RSAUtils.RSADecrypt(key.PrivateKey, en);
+            Console.WriteLine(de);
             Console.ReadKey();
+        }
+
+        public static void rsaByRandomKey(string content)
+        {
+            try
+            {
+                //Create a UnicodeEncoder to convert between byte array and string.
+                UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+                //Create byte arrays to hold original, encrypted, and decrypted data.
+                byte[] dataToEncrypt = ByteConverter.GetBytes(content);
+                byte[] encryptedData;
+                byte[] decryptedData;
+
+                //Create a new instance of RSACryptoServiceProvider to generate
+                //public and private key data.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Pass the data to ENCRYPT, the public key information
+                    //(using RSACryptoServiceProvider.ExportParameters(false),
+                    //and a boolean flag specifying no OAEP padding.
+                    encryptedData = RSAUtils.RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+
+                    //Pass the data to DECRYPT, the private key information
+                    //(using RSACryptoServiceProvider.ExportParameters(true),
+                    //and a boolean flag specifying no OAEP padding.
+                    decryptedData = RSAUtils.RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+
+                    //Display the decrypted plaintext to the console.
+                    Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
+                }
+                Console.ReadKey();
+            }
+            catch (ArgumentNullException)
+            {
+                //Catch this exception in case the encryption did
+                //not succeed.
+                Console.WriteLine("Encryption failed.");
+            }
         }
     }
 }
--- Armour/RSAUtils.cs
+++ Armour/RSAUtils.cs
@@ -4,41 +4,37 @@ using System.Text;
 
 namespace Armour
 {
-    internal class RSAUtils
+    public struct RSASecretKey
     {
-        public struct RSASecretKey
+        public RSASecretKey(string privateKey, string publicKey)
         {
-            public RSASecretKey(string privateKey, string publicKey)
-            {
-                PrivateKey = privateKey;
-                PublicKey = publicKey;
-            }
-
-            public string PublicKey { get; set; }
-            public string PrivateKey { get; set; }
-
-            public override string ToString()
-            {
-                return string.Format("PrivateKey: {0} PublicKey: {1}", PrivateKey, PublicKey);
-            }
+            PrivateKey = privateKey;
+            PublicKey = publicKey;
         }
 
-        public RSASecretKey GenerateRSASecretKey(int keySize)
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
 
+        public override string ToString()
+        {
+            return string.Format("PrivateKey: {0} PublicKey: {1}", PrivateKey, PublicKey);
+        }
+    }
+
+    internal class RSAUtils
+    {
+        public static RSASecretKey GenerateRSASecretKey(int keySize)
         {
             RSASecretKey rsaKey = new RSASecretKey();
-
             using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(keySize))
             {
-                rsaKey.PrivateKey = rsa.ToXmlString(false);
-
+                rsaKey.PrivateKey = rsa.ToXmlString(true);//true 表示同时包含 RSA 公钥和私钥；false 表示仅包含公钥。
                 rsaKey.PublicKey = rsa.ToXmlString(false);
             }
-
             return rsaKey;
         }
 
-        public string RSAEncrypt(string xmlPublicKey, string content)
+        public static string RSAEncrypt(string xmlPublicKey, string content)
         {
             string encryptedContent = string.Empty;
             using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
@@ -50,23 +46,75 @@ namespace Armour
             return encryptedContent;
         }
 
-        public string RSADecrypt(string xmlPrivateKey, string content)
+        public static string RSADecrypt(string xmlPrivateKey, string content)
 
         {
             string decryptedContent = string.Empty;
-
             using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
-
             {
                 rsa.FromXmlString(xmlPrivateKey);
-                byte[] t = Convert.FromBase64String(content);
-
-                byte[] decryptedData = rsa.Decrypt(t, false);
-
+                byte[] decryptedData = rsa.Decrypt(Convert.FromBase64String(content), false);
                 decryptedContent = Encoding.GetEncoding("gb2312").GetString(decryptedData);
             }
-
             return decryptedContent;
+        }
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This only needs
+                    //toinclude the public key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Encrypt the passed byte array and specify OAEP padding.
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            //Catch and display a CryptographicException
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This needs
+                    //to include the private key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Decrypt the passed byte array and specify OAEP padding.
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            //Catch and display a CryptographicException
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
         }
     }
 }
