38ae4611555d3d646fe0ae1db92927f5c331e591
jian zhang <18701689361@163.com>
2020-07-08 11:58:53 +0800

    11

--- Delegate/Basic.cs
+++ Delegate/Basic.cs
@@ -11,7 +11,7 @@ namespace Delegate
         //定义方法
         public static void Book(string str)
         {
-            Console.WriteLine(str);
+            Console.WriteLine(str + "test");
         }
 
         //定义同方法签名的委托
