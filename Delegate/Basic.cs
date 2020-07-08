93bf3893d43ba3d8314a8a04065425bba2c3ddef
jian zhang <18701689361@163.com>
2020-07-08 11:57:12 +0800

    111

--- Delegate/Basic.cs
+++ Delegate/Basic.cs
@@ -6,22 +6,23 @@ using System.Threading.Tasks;
 
 namespace Delegate
 {
-    class Basic
+    internal class Basic
     {
         //定义方法
         public static void Book(string str)
         {
             Console.WriteLine(str);
         }
+
         //定义同方法签名的委托
         private delegate void BuyBook(string str);
-
 
         public static string FuncBook(string str)
         {
             return str;
         }
-        public  void execute()
+
+        public void execute()
         {
             //委托起始的使用方法：
             //定义方法，定义同方法签名的委托，初始化委托实例，使用委托
@@ -36,23 +37,19 @@ namespace Delegate
             Console.WriteLine(RetBook("使用Func内置委托，含返回值"));
 
             //内置委托和匿名委托，同action
-            Func<string,string> funcAnnoyDelegate = delegate(string str)
-            {
-                return "匿名委托定义的方法"+str;
-            };
+            Func<string, string> funcAnnoyDelegate = delegate (string str)
+             {
+                 return "匿名委托定义的方法" + str;
+             };
 
             Console.WriteLine(funcAnnoyDelegate("funcValue"));
 
             //内置委托和lamda表达式
-            Func<string, string> funcLamda = (str)=>
+            Func<string, string> funcLamda = (str) =>
             {
                 return "匿名委托定义的方法" + str;
             };
             Console.WriteLine(funcLamda("funcLamda"));
         }
-
-
-
-
     }
 }
