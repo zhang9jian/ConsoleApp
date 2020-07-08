b90cfdf4ce7e7c14dc5fe47a998fbb453e5ae33f
jian zhang <18701689361@163.com>
2020-07-08 16:40:29 +0800

    qqq

---
+++ NewFeature/Default.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewFeature
+{
+    internal class Default
+    {
+        //变量赋值
+        private string s = default;
+
+        private int i = default;
+        private DateTime? dt = default;
+        private dynamic d = default;
+
+        //表达式默认值
+        public string A => default;
+
+        //方法形参可选参数赋值
+        private void Test(int a, string b = default)
+        {
+        }
+
+        //方法调用
+        public void execute()
+        {
+            Test(default, default);
+        }
+
+        //返回语句
+        private static string Test()
+        {
+            return default;
+        }
+    }
+}
